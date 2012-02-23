8fb8f7331f31c6a64cfb8caf3626c658d8174570
Wouter Dingemanse <Wouter.Dingemanse@gmail.com>
2012-02-23 20:22:54 +0100

    Refactor, theoretical better performance.

--- isometric-map/isometric-map.js
+++ isometric-map/isometric-map.js
@@ -7,18 +7,18 @@
  *
  * @author Adrian Gaudebert - adrian@gaudebert.fr
  * @version 1.0 */
-/*globals $, Crafty, console */
+/*globals $, Crafty, console, window */
 $(document).ready(function() {
-
-	var width = 1280, height = 800, spriteSize = 64, iso = Crafty.isometric.size(spriteSize);
-
-	// Create the Crafty context, with a frame rate of 50 ips, and a window size of 130px * 130px
-	Crafty.init(width, height);
-
+	
+	window.width = 1280;
+	window.height = 800;
+	window.spriteSize = 64;
+	window.iso = Crafty.isometric.size(window.spriteSize);
+	
 	/*
 	 * Declare sprite resources
 	 */
-	Crafty.sprite(spriteSize, "../resources/images/map.png", {
+	Crafty.sprite(window.spriteSize, "../resources/images/map.png", {
 		grass0 : [0, 0, 1, 1],
 		grass1 : [1, 0, 1, 1],
 		grass2 : [2, 0, 1, 1],
@@ -49,7 +49,7 @@ $(document).ready(function() {
 	 * Declare visible heights of the vertices for correct mapping
 	 * As with css its a clockwise declaration. 1 for hi, 0 for low.  
 	 */
-	var tileVisuals = [
+	window.tileVisuals = [
 						[1, 1, 1, 1], 
 						[1, 1, 1, 1], 
 						[1, 1, 1, 1], 
@@ -80,81 +80,85 @@ $(document).ready(function() {
 	 * This multidimensional array will hold the visuals for the whole map.
 	 * You can store this per tile, but I dont know how to read it out based on position.
 	 */
-	var mapArray = [];
+	window.mapArray = [];
+	
+	var generateRandomFitinTile = function(x, y, noMatch) {
+		var tileNumber = Crafty.math.randomInt(0, 23);
 
-	for(var y = 0; y < height / spriteSize * 3; y++) {
-		mapArray[y] = [];
-		for(var x = 0; x < width / spriteSize - 1; x++) {
-			var noMatch = false;
-			var tileNumber = 0;
+		//horizontal x-direction matching
+		if(x > 0) {
+			if(window.mapArray[y][x-1][1] != window.tileVisuals[tileNumber][3]) {
+				return;
+			}
+		}
+
+		//vertical y-direction matching
+		if(y > 1) {
+			if(window.mapArray[y-2][x][2] != window.tileVisuals[tileNumber][0]) {
+				return;
+			}
+		}
+
+		//if its not the first line(y=0)
+		if(y > 0) {
+			/*
+			 * On even lines, match to the upper neighbour 3th edge (visuals 3 and 2)
+			 */
+			if(y % 2 === 0) {
+
+				if(window.mapArray[y-1][x][3] != window.tileVisuals[tileNumber][0]) {
+					return;
+				}
+				if(window.mapArray[y-1][x][2] != window.tileVisuals[tileNumber][1]) {
+					return;
+				}
+			}
+			/*
+			 * On odd lines, do some odd logic.
+			 */
+			if(y % 2 === 1) {
+				if(x === 0 || !window.mapArray[y-1][x + 1]) {
+					if(window.mapArray[y-1][x][2] != window.tileVisuals[tileNumber][3]) {
+						return;
+					}
+				}
+				if(window.mapArray[y-1][x + 1]) {
+					if(window.mapArray[y-1][x+1][3] != window.tileVisuals[tileNumber][0]) {
+						return;
+					}
+					if(window.mapArray[y-1][x+1][2] != window.tileVisuals[tileNumber][1]) {
+						return;
+					}
+				}
+			}
+		}
+		return tileNumber;
+	};
+
+	// Create the Crafty context with a specific window size
+	Crafty.init(window.width, window.height);
+
+	for(var y = 0; y < window.height / window.spriteSize * 3; y++) {
+		window.mapArray[y] = [];
+		for(var x = 0; x < window.width / window.spriteSize - 1; x++) {
+			var tileNumber;
 
 			do {
-				noMatch = false; //reset to false to avoid infinite loop and too much elses.
-				tileNumber = Crafty.math.randomInt(0, 23);
-
-				//horizontal x-direction matching
-				if(x > 0) {
-					if(mapArray[y][x-1][1] != tileVisuals[tileNumber][3]) {
-						noMatch = true;
-					}
-				}
-
-				//vertical y-direction matching
-				if(y > 1) {
-					if(mapArray[y-2][x][2] != tileVisuals[tileNumber][0]) {
-						noMatch = true;
-					}
-				}
-
-				//skip matching if its already clear there is no match and if its the first line(y=0)
-				if(!noMatch && y > 0) {
-					/* 
-					 * On even lines, match to the upper neighbour 3th edge (visuals 3 and 2)
-					 */
-					if(y % 2 === 0) {
-
-						if(mapArray[y-1][x][3] != tileVisuals[tileNumber][0]) {
-							noMatch = true;
-						}
-						if(mapArray[y-1][x][2] != tileVisuals[tileNumber][1]) {
-							noMatch = true;
-						}
-					}
-					/*
-					 * On odd lines, do some odd logic. 
-					 */					 
-					if(y % 2 === 1) {
-						if(x === 0 || !mapArray[y-1][x + 1]) {
-							if(mapArray[y-1][x][2] != tileVisuals[tileNumber][3]) {
-								noMatch = true;
-							}
-						}
-						if(mapArray[y-1][x + 1]) {
-							if(mapArray[y-1][x+1][3] != tileVisuals[tileNumber][0]) {
-								noMatch = true;
-							}
-							if(mapArray[y-1][x+1][2] != tileVisuals[tileNumber][1]) {
-								noMatch = true;
-							}
-						}
-					}
-				}
-			} while(noMatch)
+				//reset to false to avoid infinite loop and too much elses.
+				tileNumber = generateRandomFitinTile(x, y);
+			} while(!tileNumber);
 
 			mapArray[y][x] = tileVisuals[tileNumber];
 
 			var tileType = 'grass' + tileNumber;
 
-			var tile = Crafty.e('2D, DOM,' + tileType)
-								.addComponent("Mouse")
-								.attr("id", String(y) + "-" + String(x))
-								.attr("name", tileType)
-								.areaMap([32, 16], [64, 32], [32, 48], [0, 32])
-								.bind('Click', function() {
-									console.log(this.attr("id"));
-								this.destroy();
+			var tile = Crafty.e('2D, DOM,' + tileType).addComponent("Mouse").attr("id", String(y) + "-" + String(x)).attr("name", tileType).areaMap([32, 16], [64, 32], [32, 48], [0, 32]).bind('Click', function() {
+				console.log(this.attr("id"));
+				console.log(iso.px2pos(128, 48));
+				this.destroy();
 			});
 			iso.place(x, y, 0, tile);
 		}
 	}
 });
+
